a2f792bb628d04dbdc1abfba6e07a830a196c44b
Telmo André Azevedo Ribeiro <61145755+TelmoRibeiro@users.noreply.github.com>
2024-06-05 17:25:21 +0100

    photos.py (fix)

--- src/photos.py
+++ src/photos.py
@@ -9,21 +9,19 @@ import threading
 def photos_control(service):
     # photo reshoot main functionality
     try:
+        photo_path = directory.PHOTO_DIR + "send.png"
         cam = Picamera2()
         cam.configure(cam.create_still_configuration(main={"format": "XRGB8888","size":(720,480)}))
         cam.start()
         while True:
-            photo_path = directory.PHOTO_DIR + "send.png"
             cam.capture_file(photo_path) # default delay = 1 sec
             sleep(1)                     # maybe not needed
     except Exception as e:
         log(service,f"detected DOWNTIME | caught {e}")
 
 def main():
-    while True:
-        photos_thread = threading.Thread(target=photos_control,args=("PHOTOS-CNTRL",))
-        photos_thread.start()
-        # RUNNING THREADS #
-        photos_thread.join()
+    photos_thread = threading.Thread(target=photos_control,args=("PHOTOS-CNTRL",))
+    photos_thread.start()
+    # RUNNING THREADS #
 
 if __name__ == "__main__": main()
